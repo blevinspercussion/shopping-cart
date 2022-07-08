377bcd387e23ad1e282316390fd7f95608f33de6
Adam Blevins <blevinspercussion@gmail.com>
2022-07-08 14:06:09 -0500

    added <Link> tag for home screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import './Components.css';
 
 import NavBar from './NavBar';
 
+import { Link } from 'react-router-dom';
+
 function Header () {
     return (
         <div className='header'>
-            <h1>The Snare Drum Store</h1>
+            <Link to="/">
+                <h1>The Snare Drum Store</h1>
+            </Link>
             <NavBar />
             
         </div>
