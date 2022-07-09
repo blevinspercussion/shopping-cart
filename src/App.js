19c71eb4e9a0329a8dfcf1d89a10592299844037
Adam Blevins <blevinspercussion@gmail.com>
2022-07-09 09:14:59 -0500

    added state (cart) and props (inventory) to App component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,39 @@ import Shop from './components/Shop';
 import Cart from './components/Cart';
 import Footer from './components/Footer';
 
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 
 function App() {
+
+  // Initialize State 
+  const [cart, setCart] = useState([]);
+
+  // Define Props
+  const inventory = [
+    {
+      name: 'Ludwig Acrolite',
+      code: 'acrolite',
+      price: 479,
+    },
+    {
+      name: 'Ludwig Black Beauty',
+      code: 'blackbeauty',
+      price: 849
+    },
+    {
+      name: 'Mapex Black Panther',
+      code: 'blackpanther',
+      price: 479,
+    },
+    {
+      name: 'DW Black Nickel',
+      code: 'dwblacknickel',
+      price: 399,
+    },
+  ]
+
+
   return (
     <div className="App">
       <Router>
