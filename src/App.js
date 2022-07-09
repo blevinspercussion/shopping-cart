ba5d923c0a93446533c6c4af916ac203dd8db12e
Adam Blevins <blevinspercussion@gmail.com>
2022-07-09 09:24:36 -0500

    added 'description' field to inventory objects in the App component

--- src/App.js
+++ src/App.js
@@ -20,21 +20,32 @@ function App() {
       name: 'Ludwig Acrolite',
       code: 'acrolite',
       price: 479,
+      description: `A classic mid-level snare drum. Perfect for when you require a sound that is both modern and vintage, 
+                    but you don't want to compromise on build quality and durability.`,
     },
     {
       name: 'Ludwig Black Beauty',
       code: 'blackbeauty',
-      price: 849
+      price: 849,
+      description: `THE classic snare drum. The original black beauty has one of the most sought after snare tones around. 
+                    Warm and metallic with just the right amount of sustain. These drums are released each year in limited 
+                    quantities.`,
     },
     {
       name: 'Mapex Black Panther',
       code: 'blackpanther',
       price: 479,
+      description: `The flagship snare drum of the Mapex brand, the black panther is truly a 'new classic'. 
+                    Using only the best modern construction techniques, the Black Panther combines a hammered 
+                    brass shell with triple-flanged hoops that results in a sound unlike any snare you're heard before.`,
     },
     {
       name: 'DW Black Nickel',
       code: 'dwblacknickel',
       price: 399,
+      description: `Sometimes called "the poor man's Black Beauty", but only by those that aren't familiar with the skill and 
+                    level of detail that the master drum builders at DW possess. The shell is constructed of a layer of black 
+                    nickel over a brass core, giving this drum a warm, round tone that is uncharacteristic of most metal snares.`,
     },
   ]
 
